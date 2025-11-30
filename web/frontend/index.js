16462a68685ead28a5aa3c9e9faf53ef77c60e0c
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-11-30 19:04:30 +1300

    Delete web/frontend/index.js

--- web/frontend/index.js
+++
@@ -1,6 +0,0 @@
-import AnalyticsPage from "./pages/Analytics";
-
-export const pages = {
-  "/": AnalyticsPage,          // Shopify loads this automatically
-  "/analytics": AnalyticsPage, // optional alias
-};
