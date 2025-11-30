709d3d340d8a784801ca24d44063e3327d55879c
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-11-30 19:11:15 +1300

    Update Navigation.jsx

--- web/frontend/src/Navigation.jsx
+++ web/frontend/src/Navigation.jsx
@@ -1,4 +1,26 @@
-{
-  label: "Analytics",
-  url: "/analytics",
+import { Navigation } from "@shopify/polaris";
+import {
+  HomeIcon,
+  ChartBarIcon,
+} from "@shopify/polaris-icons";
+
+export default function AppNavigation() {
+  return (
+    <Navigation location="/">
+      <Navigation.Section
+        items={[
+          {
+            label: "Dashboard",
+            icon: HomeIcon,
+            url: "/",
+          },
+          {
+            label: "Analytics",
+            icon: ChartBarIcon,
+            url: "/analytics",
+          },
+        ]}
+      />
+    </Navigation>
+  );
 }
