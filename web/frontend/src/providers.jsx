04ac0cfd30db205b495c94a98eed706b65bcb4e9
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-01 22:46:06 +1300

    Update providers.jsx

--- web/frontend/src/providers.jsx
+++ web/frontend/src/providers.jsx
@@ -1,28 +1,14 @@
+// CLEAN PROVIDERS — NO APP BRIDGE
+
 import React from "react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import enTranslations from "@shopify/polaris/locales/en.json";
-import {
-  Provider as AppBridgeProvider,
-  useAppBridge,
-} from "@shopify/app-bridge-react";
-
 import { BrowserRouter } from "react-router-dom";
 
 export default function Providers({ children }) {
-  const host = new URLSearchParams(location.search).get("host");
-  const apiKey = import.meta.env.VITE_SHOPIFY_API_KEY;
-
   return (
     <PolarisProvider i18n={enTranslations}>
-      <AppBridgeProvider
-        config={{
-          apiKey,
-          host,
-          forceRedirect: true,
-        }}
-      >
-        <BrowserRouter>{children}</BrowserRouter>
-      </AppBridgeProvider>
+      <BrowserRouter>{children}</BrowserRouter>
     </PolarisProvider>
   );
 }
